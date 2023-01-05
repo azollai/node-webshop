ad1e14375fd35c93238b6938f98a97fc2aed61a3
azollai <andrszollai@gmail.com>
2023-01-05 22:40:09 +0100

    init with db, product route, controller and model

---
+++ models/Product.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add name"],
+    unique: true,
+    trim: true,
+    maxlength: [50, "Name cannot be longer than 50 characters"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Product", schema);
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+
+dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+
+const products = require("./routes/products");
+
+const app = express();
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use("/api/v1/products", products);
+
+const PORT = process.env.PORT || 6000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
+
+process.on("unhandledRejection", (error, promise) => {
+  console.log(`Error: ${error.message}`);
+  server.close(() => process.exit(1));
+});
