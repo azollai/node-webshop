da035960ea6cd54ce94cdf89dcf5c95d0badbdeb
azollai <andrszollai@gmail.com>
2023-01-07 14:52:54 +0100

    fetch products on frontend

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,17 @@
 import Head from "next/head";
 
-export default function Home() {
+export async function getServerSideProps(context) {
+  const response = await fetch("http://localhost:3030/api/v1/products");
+  const products = await response.json();
+  return {
+    props: { products },
+  };
+}
+
+export default function Home({ products }) {
+  const productHtmls = products.map((product, index) => (
+    <li key={index}>{product.name} 10dkk</li>
+  ));
   return (
     <div>
       <Head>
@@ -8,7 +19,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Webshop</h1>
+      <h1>Products</h1>
+
+      <ul>{productHtmls}</ul>
     </div>
   );
 }
